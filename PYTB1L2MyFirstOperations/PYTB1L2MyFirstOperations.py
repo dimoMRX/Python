794ce6c1c4fdbcd537daaf8dd74be1c05eec95ec
omid <o.heidari20@gmail.com>
2020-10-02 12:09:52 +0200

    eerste commit vgs mij

---
+++ PYTB1L2MyFirstOperations/PYTB1L2MyFirstOperations.py
@@ -0,0 +1,14 @@
+print("hallooooo")
+A = 5
+B = 10
+C = 15
+D = 12
+E = 94
+if int(A+B+C) == 35:
+    print('dat is goed')
+elif int(A+B+C) == 60/2 and E == 100-8:
+        print('oke')
+elif 1+1+2==4 and 10-5 == int(A) and int(E+B) == int(B)+95:
+    print("heel goed")
+elif int(B*B+C) == 115 or int(B*B+C) != 115:
+    print('ahhahahahahaha')
